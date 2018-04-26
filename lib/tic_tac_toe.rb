66a13b3f33dce69a21a56f05f8d1c14b140cfe01
Wasif Khan <waasif.khaan@gmail.com>
2018-04-26 20:55:36 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,18 @@
+class TicTacToe
+  
+  def initialize(board)
+    @board=newArray(9,"")
+  end
+  
+  def position_taken?(board, index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(board,index)
+    (!position_taken?(board,index) && index.between?(0,8))? true : false
+  end
+  
+    
+    
+  
+end
